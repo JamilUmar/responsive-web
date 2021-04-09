9900fe04cf66b2fe7ce467fa3ea9a3734a04b840
Jamil Umar <jamil.umarmuhammad@gmail.com>
2021-04-09 20:42:34 +0100

    added toggle menu functionality

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const img = document.querySelector('img');
 const link = document.querySelector('ul.nav-links');
 
 img.addEventListener('click', (e) => {
-  if (link.style.display === 'none') {
-    link.style.display = 'block';
-  } else {
-    link.style.display = 'none';
+  // link.classList.replace('nav-links', 'show-menu');
+
+  if (link.className === 'nav-links') {
+    link.classList.remove('nav-links');
+    link.classList.add('show-menu');
+  } else if (link.className === 'show-menu') {
+    link.classList.remove('show-menu');
+    link.classList.add('nav-links');
   }
 });
