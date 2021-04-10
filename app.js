8ebb11f53502b7d57845ff705d5f499174b4f353
Jamil Umar <jamil.umarmuhammad@gmail.com>
2021-04-10 02:16:15 +0100

    a little fix on app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const img = document.querySelector('img');
 const link = document.querySelector('ul.nav-links');
 
 img.addEventListener('click', (e) => {
-  // link.classList.replace('nav-links', 'show-menu');
-
   if (link.className === 'nav-links') {
     link.classList.remove('nav-links');
     link.classList.add('show-menu');
